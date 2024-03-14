830ca2038684ab1e655e40275345ded120ca24be
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-14 13:23:04 +0000

    done

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,27 @@
+// let score="33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//conversion to number
+//"33"=>33
+//"3abc"=>NaN
+//true=>1; false=>0
+
+// let isLoggedIn=1;
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//""=>false
+//"sharansh"=>true
+
+// let someNumber=33
+
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
