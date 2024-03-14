5ede2e4367aab68d68639237849607972fd287b2
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-14 13:03:29 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,7 @@ let accountName='Sharansh'
 var accountHolder='Gupta'
 accountCity='Pune'
 
+/* prefer not to use var 
+ because of issue in functional and block scope*/
+
 console.table([accountId,accountName,accountHolder,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all Js code as newer version
+
+//javascript engine brownser ka andr chipa rehta hai
+
+//alert(3+3)  we are using node.js here not brownser so this will not
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>
+//symbol=>unique
+//object
+//typeof
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
