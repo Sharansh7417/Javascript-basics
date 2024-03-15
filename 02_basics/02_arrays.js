34a28431d18cb8a9aedc3c503db512d6e8d79d29
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-15 19:24:32 +0000

    done

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,28 @@
+let marvel =["Ironman","thor","spiderman"]
+let dc=["superman","batman","wonderman"]
+// marvel.push(dc)
+// console.log(marvel);
+
+
+
+// cont newheroes=marvel.concat(dc);
+
+// console.log(newheroes);
+
+// const allHeroes=[...marvel,...dc];// use of spread operator for concatening the arrays
+// console.log(allHeroes);
+
+// const arr=[1,2,3,4,5,[6,7,8],[9,10,11,[12,13,14]]];
+// const newarr=arr.flat(3);
+// console.log(newarr);
+
+// console.log(Array.isArray("hitesh"));
+// console.log(Array.from("hitesh"));
+// console.log(Array.from({name:"hitesh"}))//interesting case
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
