642dc4b6f11ff4c033b85629d9dac8f9cda75587
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-15 13:57:34 +0000

    done

---
+++ 01_basics/07_dateandTime.js
@@ -0,0 +1,29 @@
+// const date=new Date();
+// console.log(date);
+
+// console.log(date.toString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toTimeString());
+
+// const newdate=new Date(2023,2,15);
+// console.log(newdate.toLocaleDateString());
+
+// const newmethod=new Date(2024,2,15,5,3);
+// console.log(newmethod.toLocaleString());
+
+// const date=new Date("3-15-2024");
+// // console.log(date.toISOString());
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp.toFixed());
+// console.log(date.getTime());
+// console.log(Math.round(Date.now()/1000));
+
+// let newDate=new Date();
+// console.log(newDate.getMonth()+1);// as by default month is iniatilzed from 0 here in this format
+
+// newDate.toLocaleString('default',{
+//     weekday:"long"               //customize kr sktai hai using toLocalString function
+// })
