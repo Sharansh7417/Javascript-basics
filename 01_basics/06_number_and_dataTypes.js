f23b5e5767c12d189ded9a709ed03fd9b4fb887f
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-15 11:49:15 +0000

    done

---
+++ 01_basics/06_number_and_dataTypes.js
@@ -0,0 +1,24 @@
+// const num=100;
+// console.log(num);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hunreds=1000000;
+// console.log(hunreds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,3,6,8));
+// console.log(Math.round(Math.random()*6)+1);
+
+
+
