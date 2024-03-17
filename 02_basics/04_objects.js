3575a59c8f6d512999c0d658fc0ff97fdd5a83a3
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-17 13:21:52 +0000

    d

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,49 @@
+// const user =new Object();//declartation using contructor
+
+// user.id="12345";
+// user.password="#shdjd";
+
+// console.log(user);
+
+
+const mysem=Symbol("key1")
+const newsuser={
+    username:"sharansh",
+    [mysem]:"key2",
+    age:23,
+    hobbies:["music","sketching","coding","exploring"],
+
+    newdetails:{
+        fullname :{
+            firstName:"AMRITA",
+            lastName:"gupta",
+        }
+    }
+}
+
+// console.log(newsuser);
+
+// newsuser.hobbies[1]="painting";
+// console.log(newsuser);
+
+// console.log(newsuser.newdetails.fullname.firstName);
+
+
+// const obj1={a:1,b:2}
+// const obj2={c:3,d:4}
+// const obj3={e:5,f:6}
+
+// const obj={obj1,obj2}//it creates object of objects
+// console.log(obj);
+
+// const newobj=Object.assign(obj1,obj2,obj3);//it create single object
+
+// const anotherMethod={...obj1,...obj2,...obj3};
+// console.log(newobj);
+// console.log(anotherMethod);
+
+console.log(newsuser);
+console.log(Object.keys(newsuser));
+console.log(Object.values(newsuser));
+console.log(Object.entries(newsuser));
+console.log(newsuser.hasOwnProperty('username'));
