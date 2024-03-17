0c6a992f691f1dde0c2df657d2d28116fb0dbc58
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-03-17 14:35:45 +0000

    passing object

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,27 @@
+
+//
+// function dost( a ,b){
+//     return a+b;
+// }
+
+// const ans=dost(2,3);
+// console.log(ans);
+
+
+// function result(value1,value2,...num1){
+//     return num1;
+// }
+
+// let ans=result(10,20,30,40);
+// console.log(ans);
+
+const object={
+    name:'sharansh',
+    age:"23"
+}
+function lo (passobject)
+{
+    return `Hello I m ${passobject.name} and my age is ${passobject.age} `
+}
+
+console.log(lo(object));
