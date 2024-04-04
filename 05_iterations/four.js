151d82502985b3077cf685252ba024d69283fc12
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-04-04 18:57:58 +0000

    done

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -9,3 +9,21 @@
 //     console.log(`${key} is for object ${ myobject[key]}`);
 // }
 
+const programming=["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+   console.log(programming[key]);//arrays keys arein the form of indexes starting from 0
+}
+
+// const myObject={
+//     'game1':'nfs',
+//     'game2':'mario'
+// }
+
+// for (const key in myobject) {
+//     console.log(key);//it will not print anything
+        
+//     }
+
+
+//map cannot be iterated using for in loop
