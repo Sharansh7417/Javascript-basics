fa2b01b4ec7528933ba5c2c48300fa93bdff8cf9
Sharansh7417 <91908280+Sharansh7417@users.noreply.github.com>
2024-04-16 07:39:18 +0000

    keyboard done

---
+++ 07_Projects/Project5_keyboard.js
@@ -0,0 +1,19 @@
+const insert=document.querySelector('#insert');
+
+window.addEventListener('keydown',function(e){
+  insert.innerHTML=`
+  <div class='color'>
+  <table>
+  <tr>
+    <th>Key</th>
+    <th>keyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key===" "?"space":e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.Code}</td>
+  </tr>
+</table>
+  </div>`
+})
